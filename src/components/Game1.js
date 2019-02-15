cf1300afc3645667a496f808e2a4af59458baf7d
Eddie Velasco <dev@eddievelasco>
2019-02-14 16:31:42 -0800

    React-Game-Engine changes

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,25 +1,74 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import React, {Component, PureComponent} from 'react';
+import { View, Text, AppRegistry, StyleSheet, Dimensions } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {CardSection, Button} from './common';
+import { GameLoop } from "react-native-game-engine";
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
 
-class Game1 extends Component {
+class Game1 extends PureComponent {
+constructor () {
+  super();
+  this.state = {
+    x: WIDTH / 2 - RADIUS,
+    y: HEIGHT / 2 - RADIUS,
+    timePassed: false
+  };
+}
 
-onButtonPress() {
+
+updateHandler = ({ touches, screen, time }) => {
+  let move = touches.find(x => x.type === "move");
+  if (move) {
+    this.setState({
+      x: this.state.x + move.delta.pageX,
+      y: this.state.y + move.delta.pageY
+    });
+  }
+};
+//onButtonPress() {
+//  Actions.game2();
+//}
+setTimePassed() {
+  this.setState({timePassed: true});
   Actions.game2();
 }
 
+
+
   render() {
-    return(
-      <View style={{backgroundColor: '#23272A'}}>
-      <CardSection>
-        <Button onPress={this.onButtonPress.bind(this)}>
-          Game 2
-        </Button>
-      </CardSection>
-      </View>
+    if (!this.state.timePassed) {
+    setTimeout(() => {
+      this.setTimePassed();
+    }, 5000);
+  }
+
+      return (
+            <GameLoop style={styles.container} onUpdate={this.updateHandler}>
+              <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
+              <View style={{ backgroundColor: '#23272A' }} >
+              <CardSection>
+
+              </CardSection>
+              </View>
+
+            </GameLoop>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "pink",
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderRadius: RADIUS * 2
+  }
+});
 
 export default Game1;
--- src/components/Game3.js
+++ src/components/Game3.js
@@ -1,25 +1,74 @@
-import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import React, {Component, PureComponent} from 'react';
+import { View, Text, AppRegistry, StyleSheet, Dimensions } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {CardSection, Button} from './common';
+import { GameLoop } from "react-native-game-engine";
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
 
-class Game3 extends Component {
+class Game3 extends PureComponent {
+constructor () {
+  super();
+  this.state = {
+    x: WIDTH / 2 - RADIUS,
+    y: HEIGHT / 2 - RADIUS,
+    timePassed3: false
+  };
+}
 
-  onButtonPress() {
-    Actions.results();
+
+updateHandler = ({ touches, screen, time }) => {
+  let move = touches.find(x => x.type === "move");
+  if (move) {
+    this.setState({
+      x: this.state.x + move.delta.pageX,
+      y: this.state.y + move.delta.pageY
+    });
   }
+};
+//onButtonPress() {
+//  Actions.game2();
+//}
+setTimePassed() {
+  this.setState({timePassed3: true});
+  Actions.results();
+}
+
+
 
   render() {
-    return(
-      <View style={{backgroundColor: '#23272A'}}>
-      <CardSection>
-        <Button onPress={this.onButtonPress.bind(this)}>
-          Finish
-        </Button>
-      </CardSection>
-      </View>
+    if (!this.state.timePassed3) {
+    setTimeout(() => {
+      this.setTimePassed();
+    }, 5000);
+  }
+
+      return (
+            <GameLoop style={styles.container} onUpdate={this.updateHandler}>
+              <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
+              <View style={{ backgroundColor: '#23272A' }} >
+              <CardSection>
+
+              </CardSection>
+              </View>
+
+            </GameLoop>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "green",
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderRadius: RADIUS * 2
+  }
+});
 
 export default Game3;
