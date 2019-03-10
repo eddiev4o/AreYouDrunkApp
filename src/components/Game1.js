90fdd2cdd5ad6ab92113aabecf6955334c0861ee
Eddie Velasco <dev@eddievelasco>
2019-03-10 14:19:06 -0700

    Update Game1.js

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,60 +1,29 @@
 import React, {Component, PureComponent} from 'react';
-import { View, Text, AppRegistry, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, AppRegistry, StyleSheet, Dimensions, Image } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {CardSection, Button} from './common';
 import { GameLoop } from "react-native-game-engine";
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 const RADIUS = 25;
 
-class Game1 extends PureComponent {
-constructor () {
-  super();
-  this.state = {
-    x: WIDTH / 2 - RADIUS,
-    y: HEIGHT / 2 - RADIUS,
-    timePassed: false
-  };
-}
+class Game1 extends Component {
 
-
-updateHandler = ({ touches, screen, time }) => {
-  let move = touches.find(x => x.type === "move");
-  if (move) {
-    this.setState({
-      x: this.state.x + move.delta.pageX,
-      y: this.state.y + move.delta.pageY
-    });
-  }
-};
 onButtonPress() {
  Actions.game2();
 }
-// setTimePassed() {
-//   this.setState({timePassed: true});
-//   Actions.game2();
-// }
-//
-//
-//
-   render() {
-//     if (!this.state.timePassed) {
-//     setTimeout(() => {
-//       this.setTimePassed();
-//     }, 5000);
-//   }
 
+  render() {
       return (
-            <GameLoop style={styles.container} onUpdate={this.updateHandler}>
-              <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
-              <View style={{ backgroundColor: '#23272A' }} >
-              <CardSection>
-              <Button onPress={this.onButtonPress.bind(this)}>
-                Game 2
-              </Button>
-              </CardSection>
+              <View style= {{flex: 1, backgroundColor: 'blue'}}>
+                  <View style = {{flex: 2, backgroundColor:'#164170'}}>
+                  <Image source={require('../../img/RedRed.png')} style={{width: 128, height: 64}} />
+                  </View>
+                  <CardSection>
+                    <Button onPress={this.onButtonPress.bind(this)}>
+                      Game 2
+                    </Button>
+                  </CardSection>
               </View>
-
-            </GameLoop>
     );
   }
 }
@@ -66,7 +35,7 @@ const styles = StyleSheet.create({
   },
   player: {
     position: "absolute",
-    backgroundColor: "pink",
+    backgroundColor: "darkblue",
     width: RADIUS * 2,
     height: RADIUS * 2,
     borderRadius: RADIUS * 2
